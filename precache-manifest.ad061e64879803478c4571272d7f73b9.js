211db3a979054add04943f3f0076b59bee438444
nameer.as <nameer.as@pitsolutions.com>
2020-10-23 14:04:30 +0530

    Updates

---
+++ precache-manifest.ad061e64879803478c4571272d7f73b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0ad2770912828ad1b626a4fd8faaad",
+    "url": "/crwn-shop/index.html"
+  },
+  {
+    "revision": "42e25d112523716f443d",
+    "url": "/crwn-shop/static/css/main.fad78c3c.chunk.css"
+  },
+  {
+    "revision": "e61c59e681ffc62b2fa9",
+    "url": "/crwn-shop/static/js/2.5db70308.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/crwn-shop/static/js/2.5db70308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e25d112523716f443d",
+    "url": "/crwn-shop/static/js/main.2003dea9.chunk.js"
+  },
+  {
+    "revision": "c485cb5f70ecebf9a8a1",
+    "url": "/crwn-shop/static/js/runtime-main.dd21bc8c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-shop/static/media/logo.ed28accb.svg"
+  }
+]);
